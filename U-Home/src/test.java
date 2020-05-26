6be07ed22b79ce8ae22f561e433a07d4dd9c6ed5
ChenyangDu <52902587+ChenyangDu@users.noreply.github.com>
2020-05-26 21:09:43 +0800

    撤销合并

---
+++ U-Home/src/test.java
@@ -0,0 +1,13 @@
+import com.silly.repository.LodgerRepository;
+import com.silly.repository.impl.LodgerRepositoryImpl;
+
+public class test {
+    public static void main(String[] args) {
+        LodgerRepository a=new LodgerRepositoryImpl();
+        a.signin(123,"wang","123@qq.com","1575747","a12345");
+
+
+    }
+}
+
+
