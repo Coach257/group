b357079d113f00ee1337870e90011d508d6ff38d
ChenyangDu <52902587+ChenyangDu@users.noreply.github.com>
2020-05-26 20:44:00 +0800

    Update test.java
    test:添加了空行

--- U-Home/src/test.java
+++ U-Home/src/test.java
@@ -1,6 +1,7 @@
 import com.silly.repository.LodgerRepository;
 import com.silly.repository.impl.LodgerRepositoryImpl;
 
+
 public class test {
     public static void main(String[] args) {
         LodgerRepository a=new LodgerRepositoryImpl();
