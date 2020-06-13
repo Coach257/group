792c0655196749f3689b089e006fe92c7dc09168
ChenyangDu <52902587+ChenyangDu@users.noreply.github.com>
2020-06-14 01:07:45 +0800

    长期订单月初6→4

--- U-Home/src/com/silly/controller/tools/Listener.java
+++ U-Home/src/com/silly/controller/tools/Listener.java
@@ -1,9 +1,11 @@
 package com.silly.controller.tools;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
+import com.silly.entity.Order;
+import com.silly.entity.Room;
+import com.silly.service.AdminService;
+import com.silly.service.impl.AdminServiceImpl;
+
+import java.util.*;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -22,7 +24,7 @@ public class Listener implements ServletContextListener {
         int year = calendar.get(Calendar.YEAR);
         int month = calendar.get(Calendar.MONTH);
         int day = calendar.get(Calendar.DAY_OF_MONTH);
-        calendar.set(year, month, day, 8, 00, 00);
+        calendar.set(year, month, day, 1, 00, 00);
         // 当天8点（默认执行时间）
         Date defaultdate = calendar.getTime();
         Date sendDate = null;
@@ -39,25 +41,42 @@ public class Listener implements ServletContextListener {
 
             @Override
             public void run() {
+                System.out.println("refresh");
 
                 RefreshRoomEmpty.fresh();//刷新房间信息
 
                 Calendar c = Calendar.getInstance();
                 int day = c.get(Calendar.DAY_OF_MONTH);
 
+                System.out.println(day);
+                if (day == 1) {//月初
+                    refreshOrder();
+                }
                 day += 17;
 
-                if (day == c.getActualMaximum(Calendar.DAY_OF_MONTH)) {
+                if (day == c.getActualMaximum(Calendar.DAY_OF_MONTH)) {//月末
                     System.out.println("月任务执行已执行");
                     Mail.sendMial();
                 }
-
-
             }
-        }, defaultdate, 24 * 60 * 60 * 1000);// 每天执行一次检查
+        }, defaultdate, 15 * 1000);// 十秒钟检查一次
 
 
     }
 
+    private static void refreshOrder(){
+        AdminService adminService = new AdminServiceImpl();
+        List<Order> orders = adminService.AllOrder();
+        List<Room> rooms = adminService.AllRoom();
 
+        System.out.println("更新长期订单");
+        Date d = new Date();
+        System.out.println(d);
+        for(Order order : orders){
+            if(order.isTime() && order.getMode() == 6){
+                order.setMode(4);
+                adminService.ChangeOrder(order);
+            }
+        }
+    }
 }
